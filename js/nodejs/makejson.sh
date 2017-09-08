d7eb5865e719bd387a4d35c98b3065208c82850d
simonmgray <simon.gray@yale.edu>
2017-09-08 17:13:06 -0400

    Add files via upload

---
+++ js/nodejs/makejson.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+#This code takes a unix format, tab delimited file of lines 'node1  node2   strength' and generates a JSON object which is written to output.json
+
+######This code block is for user defined filename at command line interface
+#echo Please type ./filename and press enter. The file will be converted to JSON format and written as output.json
+#
+#read filename;
+######
+
+######This code is for filename given as an argument to script as follows:
+## > sh makejson.sh filename.txt
+filename=$1
+######
+
+#The input file must be sanitized as follows prior to being fed to this script: Convert to UNIX file format, remove header, add newline to end of last line (if terminal $ does not exist), write to sanitizedFile.txt
+
+#NOTE: using the < after dos2unix sends the output to stdout (instead of editing in place) so it can be piped 
+dos2unix < $filename | tail -n +2 | sed -e '$a\' > sanitizedFile.txt
+
+#Build the JSON object
+i=1
+json="{"
+while read line; do
+IFS=$'\t' read -r -a nodearray <<< "${line}";
+jsontemplate="\"key\":{\"node1\":\"n1\",\"node2\":\"n2\",\"strength\":\"X\"},";
+OUTPUT=$(echo $jsontemplate | sed -e 's/key/'"${i}"'/' -e 's/n1/'"${nodearray[0]}"'/' -e 's/n2/'"${nodearray[1]}"'/' -e 's/X/'"${nodearray[2]}"'/')
+json="$json${OUTPUT}"
+((i++));
+done < sanitizedFile.txt
+
+#Write JSON object to object.json file. NOTE: output.json has a terminal newline $
+#echo $json | sed -e 's/.$//' -e 's/$/}/' > output.json
+
+#If jq is installed do the following instead: write JSON object to object.json file. NOTE: output.json has a terminal newline. Then, beautifying output.json with jq
+echo $json | sed -e 's/.$//' -e 's/$/}/' | jq '.' > output.json
+
+
+##Try solution
+#
+#{ echo $json | sed -e 's/.$//' -e 's/$/}/' > output.json &&
+#} || {
+#echo $json | sed -e 's/.$//' -e 's/$/}/' | jq '.' > output.json
+#}
---
+++ js/nodejs/uploadFile.js
@@ -0,0 +1,56 @@
+//Code to upload a file and move it to specified path (currently /Users/simongray)
+
+var http = require('http');
+var formidable = require('formidable');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+                  if (req.url == '/fileupload') {
+                  var form = new formidable.IncomingForm();
+                  form.parse(req, function (err, fields, files) {
+                             var oldpath = files.filetoupload.path;
+                             var newpath = '/Users/simongray/Desktop/HMS_Bioinformatics/Visualization_HiC/upload_file_js/' + files.filetoupload.name;
+                             fs.rename(oldpath, newpath, function (err) {
+                                       if (err) throw err;
+                                       res.write('File uploaded and moved!');
+                                       res.end();
+                                       });
+                             });
+
+// This code works, but doesn't do what we want to pause until file exists...
+//                  if (fs.existsSync('/Users/simongray/Desktop/HMS_Bioinformatics/Visualization_HiC/upload_file_js/matrix.txt')) {
+//                  res.writeHead(200, {'Content-Type': 'text/html'});
+//                  res.write('Hello World!'); // write a response to the client
+//                  res.end(); // end the response
+//                  };
+
+//This code works by itself, but fails here because it needs to be executed after the uploaded file is moved to the directory
+//                  const { execFile } = require('child_process');
+//                  const child = execFile('./makejson.sh', ['matrix.txt'], (error, stdout, stderr) => {
+//                                         if (error) {
+//                                         throw error;
+//                                         }
+//                                         console.log(stdout);
+//                                         });
+                  
+                  } else {
+                  res.writeHead(200, {'Content-Type': 'text/html'});
+                  res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+                  res.write('<input type="file" name="filetoupload"><br>');
+                  res.write('<input type="submit">');
+                  res.write('</form>');
+                  return res.end();
+                  }
+                  }).listen(8080);
+
+
+//This code works
+//var http = require('http');
+//var fs = require('fs');
+//if (fs.existsSync('/Users/simongray/Desktop/HMS_Bioinformatics/Visualization_HiC/upload_file_js/matrix.txt')) {
+//    http.createServer(function (req, res) {
+//                      res.writeHead(200, {'Content-Type': 'text/html'});
+//                      res.write('Hello World!'); // write a response to the client
+//                      res.end(); // end the response
+//                      }).listen(8080); // the server object listens on port 8080
+//}
