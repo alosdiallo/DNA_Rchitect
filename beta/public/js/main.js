3c197a62e2d423bdb36e714cf2301220f11ebce6
simonmgray <29815977+simonmgray@users.noreply.github.com>
2018-02-14 11:42:37 -0500

    Delete main.js

--- beta/public/js/main.js
+++
@@ -1,6 +0,0 @@
-var socket = io();
-
-socket.on("loadingGIF", function() {
-          document.getElementById("cyLoading").style.visibility = "visible";
-          document.getElementById("cyIndex").style.visibility = "hidden";
-          });
