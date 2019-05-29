ec4874abd3282d3437f8eadcf75b0e4689425926
Karni Bedirian <39968446+KHBHH@users.noreply.github.com>
2019-05-29 05:13:41 +0200

    Update DNARchitect.R

--- App/DNARchitect.R
+++ App/DNARchitect.R
@@ -735,7 +735,7 @@ ui <- fluidPage(title = "Genomic Data Browser", style = "margin:15px;",
                   ),
                   tabPanel(title = "About Sample Data",
                            htmlOutput(outputId = "InfoData")
-                  ),
+                           ),
                   tabPanel(title = "Help",
                            htmlOutput('pdfviewer')
                   )
@@ -756,7 +756,7 @@ server <- function(input, output, session) {
   output$InfoData <- renderUI({
     tags$p(HTML("<hr></hr> <h3><b>K562_NHEK_GM12878-loops</h3></b><h5>Organism: Homo sapiens; Mus musculus</h5> <h5><b>Title:</b> A three-dimensional map of the human genome at kilobase resolution reveals prinicples of chromatin looping</h5>
                 <b>Summary:</b>
-                We use in situ Hi-C to probe the three-dimensional architecture of genomes, constructing haploid and diploid maps of nine cell types. The densest, in human lymphoblastoid cells, contains 4.9 billion contacts, achieving 1-kilobase resolution. We find that genomes are partitioned into local domains, which are associated with distinct patterns of histone marks and segregate into six subcompartments. We identify ~10,000 loops. These loops frequently link promoters and enhancers, correlate with gene activation, and show conservation across cell types and species. Loop anchors typically occur at domain boundaries and bind CTCF. CTCF sites at loop anchors occur predominantly (>90%) in a convergent orientation, with the asymmetric motifs 'facing' one another. The inactive X-chromosome splits into two massive domains and contains large loops anchored at CTCF-binding repeats.
+                We use in situ Hi-C to probe the three-dimensional architecture of genomes, constructing haploid and diploid maps of nine cell types. The densest, in human lymphoblastoid cells, contains 4.9 billion contacts, achieving 1-kilobase resolution. We find that genomes are partitioned into local domains, which are associated with distinct patterns of histone marks and segregate into six subcompartments. We identify ~10,000 loops. These loops frequently link promoters and enhancers, correlate with gene activation, and show conservation across cell types and species. Loop anchors typically occur at domain boundaries and bind CTCF. CTCF sites at loop anchors occur predominantly (>90%) in a convergent orientation, with the asymmetric motifs ‘facing’ one another. The inactive X-chromosome splits into two massive domains and contains large loops anchored at CTCF-binding repeats.
                 <br></br>
                 <b>Link to GEO:</b>
                 <a href = 'https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE63525' target='_blank'>https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE63525</a>
@@ -766,28 +766,28 @@ server <- function(input, output, session) {
                 Sanborn, A.L., et al. Chromatin extrusion explains key features of loop and domain formation in wild-type and engineered genomes. Proceedings of the National Academy of Sciences 2015;112(47):E6456-E6465.
                 
                 <hr></hr> <h3><b>GSM1704495_GMPro_Cap_rep1_filt5</b></h3>  <h5>Organism: Homo sapiens</h5> <h5><b>Title:</b> Capture Hi-C reveals novel candidate genes and complex long-range interactions with related autoimmune risk loci</h5>
-                <b>Summary:</b>
-                Genome-wide association studies have been tremendously successful in identifying genetic variants associated with complex diseases. The majority of association signals are intergenic and evidence is accumulating that a high proportion of signals lie in enhancer regions.We use Capture Hi-C to investigate, for the first time, the interactions between associated variants for four autoimmune diseases and their functional targets in B- and T-cell lines. Here we report numerous looping interactions and provide evidence that only a minority of interactions are common to both B- and T-cell lines, suggesting interactions may be highly cell-type specific; some disease-associated SNPs do not interact with the nearest gene but with more compelling candidate genes (for example, FOXO1, AZI2) often situated several megabases away; and finally, regions associated with different autoimmune diseases interact with each other and the same promoter suggesting common autoimmune gene targets (for example, PTPRC, DEXI and ZFP36L1).
+                  <b>Summary:</b>
+                  Genome-wide association studies have been tremendously successful in identifying genetic variants associated with complex diseases. The majority of association signals are intergenic and evidence is accumulating that a high proportion of signals lie in enhancer regions.We use Capture Hi-C to investigate, for the first time, the interactions between associated variants for four autoimmune diseases and their functional targets in B- and T-cell lines. Here we report numerous looping interactions and provide evidence that only a minority of interactions are common to both B- and T-cell lines, suggesting interactions may be highly cell-type specific; some disease-associated SNPs do not interact with the nearest gene but with more compelling candidate genes (for example, FOXO1, AZI2) often situated several megabases away; and finally, regions associated with different autoimmune diseases interact with each other and the same promoter suggesting common autoimmune gene targets (for example, PTPRC, DEXI and ZFP36L1).
                 <br></br>
-                <b>Link to GEO:</b>
-                <a href='https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE69600' target='_blank'>https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE69600</a>
-                <br></br>
-                <b>Publications:</b> <br></br>
-                Martin, P., et al. Identifying Causal Genes at the Multiple Sclerosis Associated Region 6q23 Using Capture Hi-C. PLOS ONE 2016;11(11):e0166923.<br></br>
-                Martin, P., et al. Capture Hi-C reveals novel candidate genes and complex long-range interactions with related autoimmune risk loci. Nature Communications 2015;6:10069.
-                
-                <hr></hr> <h3><b>GSM1704494_JKProCap_Rep1_Filt5</b></h3>  <h5>Organism: Homo sapiens</h5>  <h5><b>Title:</b> Capture Hi-C reveals novel candidate genes and complex long-range interactions with related autoimmune risk loci</h5>
-                <b>Summary:</b>
-                Genome-wide association studies have been tremendously successful in identifying genetic variants associated with complex diseases. The majority of association signals are intergenic and evidence is accumulating that a high proportion of signals lie in enhancer regions.We use Capture Hi-C to investigate, for the first time, the interactions between associated variants for four autoimmune diseases and their functional targets in B- and T-cell lines. Here we report numerous looping interactions and provide evidence that only a minority of interactions are common to both B- and T-cell lines, suggesting interactions may be highly cell-type specific; some disease-associated SNPs do not interact with the nearest gene but with more compelling candidate genes (for example, FOXO1, AZI2) often situated several megabases away; and finally, regions associated with different autoimmune diseases interact with each other and the same promoter suggesting common autoimmune gene targets (for example, PTPRC, DEXI and ZFP36L1).
-                <br></br>
-                <b>Link to GEO:</b>
-                <a href='https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE69600' target='_blank'>https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE69600</a>
-                <br></br>
-                <b>Publications:</b> <br></br>
-                Martin, P., et al. Identifying Causal Genes at the Multiple Sclerosis Associated Region 6q23 Using Capture Hi-C. PLOS ONE 2016;11(11):e0166923.<br></br>
-                Martin, P., et al. Capture Hi-C reveals novel candidate genes and complex long-range interactions with related autoimmune risk loci. Nature Communications 2015;6:10069.
-                
-                
+                  <b>Link to GEO:</b>
+                  <a href='https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE69600' target='_blank'>https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE69600</a>
+                  <br></br>
+                  <b>Publications:</b> <br></br>
+                  Martin, P., et al. Identifying Causal Genes at the Multiple Sclerosis Associated Region 6q23 Using Capture Hi-C. PLOS ONE 2016;11(11):e0166923.<br></br>
+                  Martin, P., et al. Capture Hi-C reveals novel candidate genes and complex long-range interactions with related autoimmune risk loci. Nature Communications 2015;6:10069.
+                  
+                  <hr></hr> <h3><b>GSM1704494_JKProCap_Rep1_Filt5</b></h3>  <h5>Organism: Homo sapiens</h5>  <h5><b>Title:</b> Capture Hi-C reveals novel candidate genes and complex long-range interactions with related autoimmune risk loci</h5>
+                  <b>Summary:</b>
+                  Genome-wide association studies have been tremendously successful in identifying genetic variants associated with complex diseases. The majority of association signals are intergenic and evidence is accumulating that a high proportion of signals lie in enhancer regions.We use Capture Hi-C to investigate, for the first time, the interactions between associated variants for four autoimmune diseases and their functional targets in B- and T-cell lines. Here we report numerous looping interactions and provide evidence that only a minority of interactions are common to both B- and T-cell lines, suggesting interactions may be highly cell-type specific; some disease-associated SNPs do not interact with the nearest gene but with more compelling candidate genes (for example, FOXO1, AZI2) often situated several megabases away; and finally, regions associated with different autoimmune diseases interact with each other and the same promoter suggesting common autoimmune gene targets (for example, PTPRC, DEXI and ZFP36L1).
+                  <br></br>
+                  <b>Link to GEO:</b>
+                  <a href='https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE69600' target='_blank'>https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE69600</a>
+                   <br></br>
+                   <b>Publications:</b> <br></br>
+                   Martin, P., et al. Identifying Causal Genes at the Multiple Sclerosis Associated Region 6q23 Using Capture Hi-C. PLOS ONE 2016;11(11):e0166923.<br></br>
+                   Martin, P., et al. Capture Hi-C reveals novel candidate genes and complex long-range interactions with related autoimmune risk loci. Nature Communications 2015;6:10069.
+                   
+
                 <hr></hr> <h3><b>ImmGen_T.Nve.Sp.ATAC.bg | ImmGen_Treg.Sp.ATAC.bg</b></h3> <h5>Organism: Mus musculus</h5> <h5><b>Title:</b> ImmGen ATAC-seq data</h5>
                 <b>Summary:</b>
                 Immunological Genome Project chromatin accessibility maps for 86 different immunocytes (ATAC-seq). Immune cell populations were isolated in high-purity by flow cytometry.
@@ -800,8 +800,8 @@ server <- function(input, output, session) {
                 <b>Publications:</b><br></br>
                 Yoshida, H., Lareau, C. A., Ramirez, R. N., Rose, S. A., Maier, B., Wroblewska, A., . . . Benoist, C. (2019). The cis-Regulatory Atlas of the Mouse Immune System. Cell, 176(4), 897-912.e820. doi:<a href='https://doi.org/10.1016/j.cell.2018.12.036' target='_blank'>https://doi.org/10.1016/j.cell.2018.12.036</a>
                 "
-    ))
-  })
+                ))
+    })
   
   ## Go to the "Plots" tab when click "goToPlots" button
   observeEvent(input$goToPlots, {
@@ -905,14 +905,14 @@ server <- function(input, output, session) {
       shinyjs::hide(id = "mRNAPlot")
       #shinyjs::hide(id = "mrnaPlot")
     }
-    # if ("HiC" %in% input$fileTypes && "ChIP" %in% input$fileTypes){
-    #    updateSelectizeInput(session = session, inputId = "Load",  choices = c("K562_NHEK_GM12878-loops", "GSM1704495_GMPro_Cap_rep1_filt5", "GSM1704494_JKProCap_Rep1_Filt5", "FoxP3_ChIP-seq_Peaks.bed"), 
-    #                         options = list(maxOptions = 3, placeholder = 'Type file name', onInitialize = I('function() { this.setValue(""); }')),selected = NULL)
-    #  }
-    #  if ("HiC" %in% input$fileTypes && "ATAC" %in% input$fileTypes){
-    #    updateSelectizeInput(session = session, inputId = "Load",  choices = c("K562_NHEK_GM12878-loops", "GSM1704495_GMPro_Cap_rep1_filt5", "GSM1704494_JKProCap_Rep1_Filt5", "ImmGen_T.Nve.Sp.ATAC.bg", "ImmGen_Treg.Sp.ATAC.bg"), 
-    #                         options = list(maxOptions = 3, placeholder = 'Type file name', onInitialize = I('function() { this.setValue(""); }')),selected = NULL)
-    #  }
+   # if ("HiC" %in% input$fileTypes && "ChIP" %in% input$fileTypes){
+  #    updateSelectizeInput(session = session, inputId = "Load",  choices = c("K562_NHEK_GM12878-loops", "GSM1704495_GMPro_Cap_rep1_filt5", "GSM1704494_JKProCap_Rep1_Filt5", "FoxP3_ChIP-seq_Peaks.bed"), 
+  #                         options = list(maxOptions = 3, placeholder = 'Type file name', onInitialize = I('function() { this.setValue(""); }')),selected = NULL)
+  #  }
+  #  if ("HiC" %in% input$fileTypes && "ATAC" %in% input$fileTypes){
+  #    updateSelectizeInput(session = session, inputId = "Load",  choices = c("K562_NHEK_GM12878-loops", "GSM1704495_GMPro_Cap_rep1_filt5", "GSM1704494_JKProCap_Rep1_Filt5", "ImmGen_T.Nve.Sp.ATAC.bg", "ImmGen_Treg.Sp.ATAC.bg"), 
+  #                         options = list(maxOptions = 3, placeholder = 'Type file name', onInitialize = I('function() { this.setValue(""); }')),selected = NULL)
+  #  }
     
   })
   
@@ -984,11 +984,11 @@ server <- function(input, output, session) {
                            
                            # # Output: Data file ----
                            tableOutput(paste0(input$fileTypes[i],"Table"))
-                           ##\ div(id = "aa", style="display:none",
-                           # wellPanel(
-                           ##       htmlOutput(outputId = "InfoData")
-                           #)
-                           ## )
+                          ##\ div(id = "aa", style="display:none",
+                              # wellPanel(
+                          ##       htmlOutput(outputId = "InfoData")
+                              #)
+                          ## )
                  )
                )
       )
@@ -1096,7 +1096,7 @@ server <- function(input, output, session) {
     
     #Read all data files into memory...
     withProgress(message = "Processing Data", value = 0.10, {
-      
+
       if("mRNA" %in% input$fileTypes){
         req(input$mRNAFile)
         mRNAdata <- mRNAdataRead(input);
@@ -1113,24 +1113,38 @@ server <- function(input, output, session) {
           switch (input$Load,
                   "K562_NHEK_GM12878-loops" = {
                     HiCdata <- sampleData2
-                    # div(id = "aa",
-                    # )
+                    output$HiCTable <- renderTable({
+                      if(eval(parse(text = (paste0("input$", "HiC", "Disp")))) == "head") {
+                        head(HiCdata) 
+                      }
+                      else {
+                        tail(HiCdata) 
+                      }
+                    })
                   },
                   "GSM1704495_GMPro_Cap_rep1_filt5" = {
                     HiCdata <- read.delim("https://storage.googleapis.com/gencode_ch_data/Sample_Data/datasets/GSM1704495_GMPro_Cap_rep1_filt5.bedpe")
+                    output$HiCTable <- renderTable({
+                      if(eval(parse(text = (paste0("input$", "HiC", "Disp")))) == "head") {
+                        head(HiCdata) 
+                      }
+                      else {
+                        tail(HiCdata) 
+                      }
+                    })
                   },
                   "GSM1704494_JKProCap_Rep1_Filt5" = {
                     HiCdata <- read.delim("https://storage.googleapis.com/gencode_ch_data/Sample_Data/datasets/GSM1704494_JKProCap_Rep1_Filt5.bedpe")
+                    output$HiCTable <- renderTable({
+                      if(eval(parse(text = (paste0("input$", "HiC", "Disp")))) == "head") {
+                        head(HiCdata) 
+                      }
+                      else {
+                        tail(HiCdata) 
+                      }
+                    })
                   }
           )
-          output$HiCTable <- renderTable({
-            if(eval(parse(text = (paste0("input$", "HiC", "Disp")))) == "head") {
-              head(HiCdata) 
-            }
-            else {
-              tail(HiCdata) 
-            }
-          })
         }
         else{
           req(input$HiCFile)
@@ -1152,20 +1166,27 @@ server <- function(input, output, session) {
           switch (input$Load,
                   "ImmGen_T.Nve.Sp.ATAC.bg" = {
                     ATACdata <- read.delim("https://storage.googleapis.com/gencode_ch_data/Sample_Data/datasets/Tcon_Immgen_3.bg")
+                    output$ATACTable <- renderTable({
+                      if(eval(parse(text = (paste0("input$", "ATAC", "Disp")))) == "head") {
+                        head(ATACdata) 
+                      }
+                      else {
+                        tail(ATACCdata) 
+                      } 
+                    })
                   },
                   "ImmGen_Treg.Sp.ATAC.bg" = {
                     ATACdata <- read.delim("https://storage.googleapis.com/gencode_ch_data/Sample_Data/datasets/Treg_Immgen_3.bg")
+                    output$ATACTable <- renderTable({
+                      if(eval(parse(text = (paste0("input$", "ATAC", "Disp")))) == "head") {
+                        head(ATACdata) 
+                      }
+                      else {
+                        tail(ATACCdata) 
+                      } 
+                    })
                   }
           )
-          output$ATACTable <- renderTable({
-            if(eval(parse(text = (paste0("input$", "ATAC", "Disp")))) == "head") {
-              head(ATACdata) 
-            }
-            else {
-              tail(ATACCdata) 
-            } 
-          })
-          
         }
         else{
           req(input$ATACFile)
@@ -1176,11 +1197,11 @@ server <- function(input, output, session) {
         }
         #checkHeader(data=ATACdata, dataFileType="ATAC", input)
       }
-      
+
       
       #Increment progress
       incProgress(0.75)
-      
+
       if("ChIP" %in% input$fileTypes){
         if(is.null(input$ChIPFile)){
           #switch (input$Load,
@@ -2074,7 +2095,7 @@ server <- function(input, output, session) {
   session$allowReconnect(TRUE)
   options(rsconnect.error.trace = TRUE)
   
-  }
+}
 shinyApp(ui = ui, server = server)
 
 
